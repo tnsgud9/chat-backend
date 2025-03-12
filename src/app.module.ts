a92ebfbc6cccd8c87033aedcd852d4ebd9d4c500
SoonHyung_LEE <tnsgud9@naver.com>
2025-03-12 18:56:10 +0900

    config directory architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ChatModule, DatabaseModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
