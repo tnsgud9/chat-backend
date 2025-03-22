48013ac9f4135a2b77d2bf38136d4836630c23b6
SoonHyung_LEE <tnsgud9@naver.com>
2025-03-22 19:30:36 +0900

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './user/user.module';
 import { ChatModule } from './chat/chat.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UsersModule, ChatModule, DatabaseModule, ConfigModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ChatModule,
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
