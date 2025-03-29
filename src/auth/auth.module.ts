b6ece5be68d84a64cc8aaf5c24b90c07c7d6d783
SoonHyung_Lee <tnsgud9@naver.com>
2025-03-29 23:00:52 +0900

    config up

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,16 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthAccessTokenGuard } from './auth.guard';
 import { AuthAccessTokenStrategy } from './auth.strategy';
+import { ConfigService } from 'src/config/config.service';
 
 @Module({
   imports: [JwtModule.register({ global: true })],
   controllers: [AuthController],
-  providers: [AuthService, AuthAccessTokenGuard, AuthAccessTokenStrategy],
+  providers: [
+    AuthService,
+    AuthAccessTokenGuard,
+    AuthAccessTokenStrategy,
+    ConfigService,
+  ],
 })
 export class AuthModule {}
