649fd7fb32a6717d8923825c16e9dd1b09a6a47f
SoonHyung_LEE <tnsgud9@naver.com>
2025-03-29 13:54:13 +0900

    dto validaiton 추가

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,20 +1,53 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
 export class AuthLoginRequestDto {
+  @IsEmail()
   id: string;
+
+  @IsString()
+  @Length(8, 256)
+  @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,20}$/)
   password: string;
 }
 
 export class AuthLoginResponseDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 20)
+  @Matches(/^[a-zA-Z0-9_]+$/)
   nickname: string;
+
   accessToken: string;
 }
 
 export class AuthSignupRequestDto {
+  @IsEmail()
   id: string;
+
+  @IsString()
+  @Length(8, 256)
+  @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,20}$/)
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 20)
+  @Matches(/^[a-zA-Z0-9_]+$/)
   nickname: string;
 }
 
 export class AuthSignupResponseDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 20)
+  @Matches(/^[a-zA-Z0-9_]+$/)
   nickname: string;
+
   accessToken: string;
 }
