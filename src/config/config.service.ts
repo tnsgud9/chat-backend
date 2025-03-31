814ab9a85cb3feec8909fd1032f92997e57ac438
Lee_SoonHyung <tnsgud9@naver.com>
2025-03-31 15:28:58 +0900

    refact config service

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -7,9 +7,12 @@ export class ConfigService {
   config: Configuration;
   constructor(private configService: NestConfigService) {
     this.config = {
-      DB_URI: configService.get('DB_URI') || 'localhost',
-      PORT: parseInt(configService.get('PORT') || '27017'),
-      SECRET_TOKEN: configService.get('SECRET_TOKEN') || 'secretsecretsecret',
+      DB_URI: this.configService.get('DB_URI', 'localhost'),
+      PORT: this.configService.get<number>('PORT', 3000),
+      SECRET_TOKEN: this.configService.get(
+        'SECRET_TOKEN',
+        'secretsecretsecret',
+      ),
     } as Configuration;
   }
 }
