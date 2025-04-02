aec0bd14fc8b102f1cdd14b62e19446df62c7dcd
Lee_SoonHyung <tnsgud9@naver.com>
2025-04-02 16:21:38 +0900

    fix base Schema

--- src/database/schema/base.schema.ts
+++ src/database/schema/base.schema.ts
@@ -1,13 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid'; // uuidv4 임포트
 
-@Schema({ timestamps: true })
+@Schema({ timestamps: true, id: false }) // 기본 _id 사용 안 함
 export class BaseSchema extends Document {
-  @Prop({ required: true, default: Date.now })
-  createdAt: Date;
+  @Prop({ type: String, default: uuidv4 }) // uuidv4로 기본값 설정
+  public uid: string; // uuidv4를 기준으로 생성된 uid 필드
 
-  @Prop({ required: true, default: Date.now })
-  updatedAt: Date;
+  // createdAt, updatedAt은 Mongoose가 자동으로 관리
 }
 
 export const BaseSchemaFactory = SchemaFactory.createForClass(BaseSchema);
