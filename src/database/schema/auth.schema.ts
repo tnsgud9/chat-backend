ff04ade77d34696fc470fbce5ecec0c35e33c6e2
SoonHyung_LEE <tnsgud9@naver.com>
2025-04-05 17:58:46 +0900

    스키마 구조화

--- src/database/schema/auth.schema.ts
+++ src/database/schema/auth.schema.ts
@@ -3,14 +3,22 @@ import { HydratedDocument } from 'mongoose';
 import { Permission } from 'src/common/enums/permission.enum';
 import { BaseSchema } from './base.schema';
 
+// 'Auth' 모델의 문서 타입을 정의
+// HydratedDocument는 Mongoose에서 MongoDB에서 로드된 후의 문서를 나타낸다.
 export type AuthDocument = HydratedDocument<Auth>;
 
+// Auth 스키마 정의
 @Schema()
 export class Auth extends BaseSchema {
+  // 필수, 고유한 사용자명
   @Prop({ required: true, unique: true })
   username: string;
+
+  // 필수 비밀번호
   @Prop({ required: true })
   password: string;
+
+  // 필수, 권한 열거형 (기본값: USER)
   @Prop({
     required: true,
     type: String,
@@ -18,8 +26,12 @@ export class Auth extends BaseSchema {
     default: Permission.USER,
   })
   permission: Permission;
-  @Prop()
+
+  // 닉네임
+  @Prop({ required: true })
   nickname: string;
 }
 
+// Auth 클래스를 바탕으로 Mongoose 스키마를 생성
+// 'SchemaFactory.createForClass(Auth)'는 'Auth' 클래스를 Mongoose에서 사용할 수 있는 스키마 객체로 변환
 export const AuthSchema = SchemaFactory.createForClass(Auth);
--- src/database/schema/base.schema.ts
+++ src/database/schema/base.schema.ts
@@ -1,13 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { v4 as uuidv4 } from 'uuid'; // uuidv4 임포트
+import { v4 as uuidv4 } from 'uuid';
 
-@Schema({ timestamps: true, id: false }) // 기본 _id 사용 안 함
+@Schema({ timestamps: true }) // createdAt과 updatedAt을 자동으로 관리
 export class BaseSchema extends Document {
-  @Prop({ type: String, default: uuidv4 }) // uuidv4로 기본값 설정
+  @Prop({ default: uuidv4, unique: true }) // 기본값을 UUIDv4로 설정
   public uid: string; // uuidv4를 기준으로 생성된 uid 필드
 
   // createdAt, updatedAt은 Mongoose가 자동으로 관리
 }
 
 export const BaseSchemaFactory = SchemaFactory.createForClass(BaseSchema);
+
+BaseSchemaFactory.pre('save', function (next) {
+  if (!this.uid) {
+    this.uid = uuidv4(); // uid가 없으면 UUIDv4를 생성하여 할당
+  }
+  next();
+});
